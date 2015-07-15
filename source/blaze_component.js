1ace2ad50df387a8aa0dfa9bf214aa2e6f815c07
Dominik Guzei <dominik.guzei@gmail.com>
2015-07-15 19:00:37 +0200

    refactors blaze component code to javascript

---
+++ source/blaze_component.js
@@ -0,0 +1,37 @@
+if(Package['peerlibrary:blaze-components'] !== null) { // weak dependency
+  var BlazeComponent = Package['peerlibrary:blaze-components'].BlazeComponent;
+
+  Space.ui.BlazeComponent = Space.Object.extend();
+
+  // Make it a Blaze Component by copying over static and prototype properties
+  for(var property in BlazeComponent) {
+    if(property !== '__super__') {
+      Space.ui.BlazeComponent[property] = BlazeComponent[property];
+    }
+  }
+  for(var property in BlazeComponent.prototype) {
+    if(property !== 'constructor') {
+      var value = BlazeComponent.prototype[property];
+      Space.ui.BlazeComponent.prototype[property] = value;
+    }
+  }
+
+  // Mixin convenient dependencies and make components stateful
+  Space.ui.BlazeComponent.mixin({
+
+    Dependencies: {
+      eventBus: 'Space.messaging.EventBus'
+    },
+
+    onCreated: function() {
+      this.constructor.Application.injector.injectInto(this);
+      this.setupState();
+    },
+
+    publish: function (event) {
+      this.eventBus.publish(event);
+    }
+  });
+
+  Space.ui.BlazeComponent.mixin(Space.ui.Stateful);
+}
